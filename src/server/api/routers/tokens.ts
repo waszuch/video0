f6177bea166616785561b7193c35b8dea96d0bc2
Łukasz Cybulski <cybulski.lukasz@icloud.com>
2025-04-16 21:21:18 +0200

    fix: cr

--- src/server/api/routers/tokens.ts
+++ src/server/api/routers/tokens.ts
@@ -1,122 +1,94 @@
-import { FREE_INITIAL_TOKEN_AMOUNT, POLAR_PRODUCT_IDS } from "@/lib/contants";
-import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
-import { db } from "@/server/db";
-import {
-  generationTokens,
-  generationTransactions,
-  type DBGenerationToken,
-} from "@/server/db/schema";
-import { polar } from "@/utils/polar";
 import { TRPCError } from "@trpc/server";
 import { eq, sql } from "drizzle-orm";
 import { Z } from "node_modules/@upstash/redis/zmscore-CjoCv9kz.mjs";
 import { z } from "zod";
+import { FREE_INITIAL_TOKEN_AMOUNT, POLAR_PRODUCT_IDS } from "@/lib/contants";
+import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
+import { db } from "@/server/db";
+import {
+	type DBGenerationToken,
+	generationTokens,
+	generationTransactions,
+} from "@/server/db/schema";
+import { polar } from "@/utils/polar";
 
 export const tokensRouter = createTRPCRouter({
-  get: privateProcedure.query(async ({ ctx }) => {
-    const { user } = ctx;
-    if (!user) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
+	get: privateProcedure.query(async ({ ctx }) => {
+		const { user } = ctx;
+		if (!user) {
+			throw new TRPCError({ code: "UNAUTHORIZED" });
+		}
 
-    let tokens: Partial<DBGenerationToken> | undefined =
-      await db.query.generationTokens.findFirst({
-        where: eq(generationTokens.profileId, user.id),
-        columns: {
-          availableTokens: true,
-        },
-      });
+		let tokens: Partial<DBGenerationToken> | undefined =
+			await db.query.generationTokens.findFirst({
+				where: eq(generationTokens.profileId, user.id),
+				columns: {
+					availableTokens: true,
+				},
+			});
 
-    if (!tokens) {
-      tokens = (
-        await db
-          .insert(generationTokens)
-          .values({
-            id: crypto.randomUUID(),
-            profileId: user.id,
-            availableTokens: FREE_INITIAL_TOKEN_AMOUNT,
-            initialTokenAmount: FREE_INITIAL_TOKEN_AMOUNT,
-          })
-          .returning()
-      )?.[0];
-    }
+		if (!tokens) {
+			tokens = (
+				await db
+					.insert(generationTokens)
+					.values({
+						id: crypto.randomUUID(),
+						profileId: user.id,
+						availableTokens: FREE_INITIAL_TOKEN_AMOUNT,
+						initialTokenAmount: FREE_INITIAL_TOKEN_AMOUNT,
+					})
+					.returning()
+			)?.[0];
+		}
 
-    if (!tokens) {
-      throw new TRPCError({ code: "NOT_FOUND" });
-    }
+		if (!tokens) {
+			throw new TRPCError({ code: "NOT_FOUND" });
+		}
 
-    return tokens.availableTokens;
-  }),
-  use: privateProcedure.mutation(async ({ ctx, input }) => {
-    const { user } = ctx;
-    if (!user) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
+		return tokens.availableTokens;
+	}),
+	checkout: privateProcedure
+		.input(
+			z.object({
+				chatId: z.string(),
+			}),
+		)
+		.mutation(async ({ ctx, input }) => {
+			try {
+				const customer = await polar.customers.getStateExternal({
+					externalId: ctx.user.id,
+				});
 
-    await db.transaction(async (tx) => {
-      const token = await tx.query.generationTokens.findFirst({
-        where: eq(generationTokens.profileId, user.id),
-      });
-      if (!token) {
-        throw new TRPCError({ code: "NOT_FOUND" });
-      }
+				const checkoutUrl = await polar.checkouts.create({
+					customerId: customer.id,
+					customerExternalId: ctx.user.id,
+					successUrl: `https://video0.dev/chat/${input.chatId}`,
+					products: [
+						POLAR_PRODUCT_IDS["3_TOKENS"].id,
+						POLAR_PRODUCT_IDS["5_TOKENS"].id,
+						POLAR_PRODUCT_IDS["10_TOKENS"].id,
+					],
+				});
 
-      await tx
-        .update(generationTokens)
-        .set({
-          availableTokens: sql`${generationTokens.availableTokens} - 1`,
-        })
-        .where(eq(generationTokens.profileId, user.id));
+				return checkoutUrl;
+			} catch (error) {
+				const customer = await polar.customers.create({
+					email: ctx.user.email ?? "",
+					externalId: ctx.user.id,
+				});
 
-      await tx.insert(generationTransactions).values({
-        id: crypto.randomUUID(),
-        generationTokenId: token.id,
-        amount: 1,
-      });
-    });
-  }),
-  checkout: privateProcedure
-    .input(
-      z.object({
-        chatId: z.string(),
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      try {
-        const customer = await polar.customers.getStateExternal({
-          externalId: ctx.user.id,
-        });
+				const checkoutUrl = await polar.checkouts.create({
+					customerId: customer.id,
+					customerExternalId: ctx.user.id,
+					successUrl: `https://video0.dev/chat/${input.chatId}`,
+					products: [
+						POLAR_PRODUCT_IDS["3_TOKENS"].id,
+						POLAR_PRODUCT_IDS["5_TOKENS"].id,
+						POLAR_PRODUCT_IDS["10_TOKENS"].id,
+					],
+				});
 
-        const checkoutUrl = await polar.checkouts.create({
-          customerId: customer.id,
-          customerExternalId: ctx.user.id,
-          successUrl: `https://video0.dev/chat/${input.chatId}`,
-          products: [
-            POLAR_PRODUCT_IDS["3_TOKENS"].id,
-            POLAR_PRODUCT_IDS["5_TOKENS"].id,
-            POLAR_PRODUCT_IDS["10_TOKENS"].id,
-          ],
-        });
-
-        return checkoutUrl;
-      } catch (error) {
-        const customer = await polar.customers.create({
-          email: ctx.user.email ?? "",
-          externalId: ctx.user.id,
-        });
-
-        const checkoutUrl = await polar.checkouts.create({
-          customerId: customer.id,
-          customerExternalId: ctx.user.id,
-          successUrl: `https://video0.dev/chat/${input.chatId}`,
-          products: [
-            POLAR_PRODUCT_IDS["3_TOKENS"].id,
-            POLAR_PRODUCT_IDS["5_TOKENS"].id,
-            POLAR_PRODUCT_IDS["10_TOKENS"].id,
-          ],
-        });
-
-        return checkoutUrl;
-      }
-    }),
+				return checkoutUrl;
+			}
+		}),
 });
